234f35579cb726bd4209d9290155d9085e497f65
jesus montes <kexuia@hotmail.com>
2024-06-06 10:55:15 +0000

    modificado sessions,eliminado dim_events y generado count_events_by_user. Creada data_time_sessions

---
+++ models/marts/data_time_sessions.sql
@@ -0,0 +1,21 @@
+WITH sessions_cte AS (
+    SELECT * FROM {{ref('stg_sql_server_dbo__sessions')}}
+),
+data_time_sessions_cte AS (
+   SELECT 
+    user_id,
+    session_id,
+    init_session_time,
+    finish_session_time,
+    time_checkout,
+    time_package_shipped,
+    TIMESTAMPDIFF('minute',init_session_time,time_checkout) as duration_session_init_to_checkout_min,
+    TIMESTAMPDIFF('minute',time_checkout,time_package_shipped) as duration_preparing_to_shipped_min,
+    TIMESTAMPDIFF('minute',init_session_time,finish_session_time) as duration_session_total_min,
+    TIMESTAMPDIFF('hour',time_checkout,time_package_shipped) as duration_preparing_to_shipped_hour,
+    TIMESTAMPDIFF('day',time_checkout,time_package_shipped) as duration_preparing_to_shipped_day
+    
+    FROM sessions_cte
+    
+)
+SELECT * FROM data_time_sessions_cte
--- models/staging/sql_server_dbo/stg_sql_server_dbo__sessions.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__sessions.sql
@@ -2,9 +2,11 @@ WITH sessions AS (
     SELECT 
     user_id,
     session_id,
-    MIN(created_at_utc) AS init_time,
-    MAX (created_at_utc) AS finish_time,
-    TIMESTAMPDIFF('minute',MIN(created_at_utc),MAX(created_at_utc)) as duration_session_min 
+    MIN(created_at_utc) AS init_session_time,
+    MAX (created_at_utc) AS finish_session_time,
+    MAX(CASE WHEN event_type = 'checkout' THEN created_at_utc END) AS time_checkout,
+    MAX(CASE WHEN event_type = 'package_shipped' THEN created_at_utc END) AS time_package_shipped,
+    --TIMESTAMPDIFF('minute',MIN(created_at_utc),MAX(created_at_utc)) as duration_session_min 
     FROM {{ref('stg_sql_server_dbo__events')}}
     GROUP BY 1,2
     
